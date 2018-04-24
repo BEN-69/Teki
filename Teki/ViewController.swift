ecc323de437340fb543f609abe91c42021594120
benaata <r.benaata@gmail.com>
2018-04-24 09:34:16 +0200

    Premier commit

--- Teki/ViewController.swift
+++ Teki/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var celebrities = ["Email", "Télephone", "Site web"]
+    var activities = ["r.benaata@gmail.com", "0627715477", "http://brportfolio.pe.hu"]
+    
+    @IBOutlet weak var homeTextView: UITextView!
+    @IBOutlet weak var homeLabel: UILabel!
+    @IBOutlet weak var hello: UILabel!
+    
+    @IBAction func contact() {
+        
+        homeTextView.backgroundColor = UIColor .white ;
+    homeTextView.layer.borderWidth=1
+        homeTextView.layer.cornerRadius = 8
+    homeTextView.layer.borderColor =
+        UIColor .blue .cgColor
+        
+        
+        // On séléctionne un élément alétoire parmi les célébrités
+        let randomIndex1 = Int(arc4random_uniform(UInt32(celebrities.count)))
+        let celebrity = celebrities[randomIndex1]
+      
+        
+        // On séléctionne un élément aléatoire parmi les activités
+        let activity = activities[randomIndex1]
+                
+        hello.text = " " + celebrity + " :  " + activity + " !"
+        hello.backgroundColor =
+        UIColor .yellow
+        hello.layer.borderColor =
+            UIColor .red .cgColor
+
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
